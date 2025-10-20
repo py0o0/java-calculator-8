484c3c623321cf7ab57f3c2d7e0460f2ce6e11e0
py0o0 <pantom0425@naver.com>
2025-10-20 14:53:46 +0900

    feat: 잘못된 형식의 커스텀 구분자 예외 처리

--- src/main/java/calculator/DelimiterParser.java
+++ src/main/java/calculator/DelimiterParser.java
@@ -6,7 +6,12 @@ public class DelimiterParser {
 
     public DelimiterParser(String input) {
         if (input.startsWith("//")) {
-            String[] temp = input.split("\\\\n", 2);
+            String[] temp = input.split("\\\\n", 2); // \n이 커스텀 구분자일 수도 있으니 limit 2
+
+            if(temp.length < 2) {
+                throw new IllegalArgumentException("숫자가 포함되어 있지 않습니다.");
+            }
+
             delimiter = ",|:" + "|" + temp[0].substring(2);
             rawNumbers = temp[1];
         } else {
